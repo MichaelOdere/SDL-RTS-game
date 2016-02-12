b9a2d90987de4aa0a6ff99482dd5fd0e3e72da6c
Sam Cordes <swc@uoregon.edu>
2016-02-11 21:20:34 -0800

    Moved much of code in main.cpp to new Character class in order to soon start creating multiple units

--- character.hpp
+++ character.hpp
@@ -1,42 +1,45 @@
-#ifndef MAIN_HPP_INCLUDED
-#define MAIN_HPP_INCLUDED
+
+#ifndef CHARACTER_HPP_INCLUDED
+#define CHARACTER_HPP_INCLUDED
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
 #include "sdl_setup.hpp"
 #include "sprite.hpp"
-#include "character.hpp"
+#include <math.h>
 
-class Main
+class Character
 {
 public:
-    Main();
-    ~Main();
-
-    void GameLoop();
+    Character(SDL_Setup* passed_SDL_Setup, int *passed_MouseX, int *passed_MouseY);
+    ~Character();
 
     double GetDistance(int x1, int y1, int x2, int y2);
 
+    void Update();
+    void Draw();
+
 private:
 
-    Character *unit;
-    bool quit;
-
-    Sprite* grass;
+    int *MouseX;
+    int *MouseY;
 
     SDL_Setup* sdl_setup;
 
+    Sprite* unit;
+
     int timeCheck;
-    int MouseX;
-    int MouseY;
+
     bool follow;
     int follow_point_x;
     int follow_point_y;
+
     float angle;
+
     float distance;
     bool stopAnimation;
 
 };
 
 
-#endif // MAIN_HPP_INCLUDED
+#endif // CHARACTER_HPP_INCLUDED
